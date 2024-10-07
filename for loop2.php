ee8fbcc6fe246df7f669afc87ff541da2400d8ca
Noman Mia <nomanislamnishad@gmail.com>
2024-10-07 10:01:57 +0600

    added loops

---
+++ for loop2.php
@@ -0,0 +1,20 @@
+<?php
+
+$conutStars = 20;
+
+echo"heading heading heading heading \n";
+for ($i = 0; $i <= $conutStars; $i++){
+    for($j = 0; $j <= $i; $j++) {
+      echo"*";
+    }
+    echo"\n";
+
+    }
+
+    echo "\n";
+echo "This is a paragraph. This is a paragraph. This is a paragraph. This is a paragraph. This is a paragraph. \n";
+for($i = 1; $i <= $conutStars; $i++){
+    echo "*";
+}
+echo "\n";
+echo "Signature\n";
